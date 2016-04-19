c8bca7befaed452f8541ae571d79f45a14165b86
hambone124 <hperez@tervis.com>
2016-04-19 16:41:23 -0400

    Added Kibana automatic package

---
+++ AutomaticPackages/kibana/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿<#
+Script modified from Dmitry Kireev's original script.
+https://chocolatey.org/packages/logstash-contrib
+#>
+
+
+$PackageName = '{{PackageName}}'
+$PackageVersion = '{{PackageVersion}}'
+$PackageDirectory="$env:ProgramFiles\$PackageName"
+$DownloadURL = "https://download.elastic.co/$PackageName/$PackageName/$PackageName-$PackageVersion-windows.zip"
+
+Write-Host "Making sure there are no traces of an old installation. Cleaning up $PackageDirectory."
+
+$DoesPackagePathExist = Test-Path $(Join-Path $PackageDirectory "$PackageName*")
+
+if ($DoesPackagePathExist -eq $true) {
+    Get-ChildItem "${PackageDirectory}" | Where-Object {$_.Name -ilike "$PackageName*"} | Remove-Item -Recurse
+    Get-ChildItem "${PackageDirectory}" | Where-Object {$_.Name -ilike "${PackageName}Install*"} | Remove-Item
+}
+else {
+    Write-Host "No old files to remove."
+}
+
+Get-ChocolateyWebFile "${PackageName}" $(Join-Path $PackageDirectory "${PackageName}Install.zip") $DownloadURL $DownloadURL
+Get-ChocolateyUnzip $(Join-Path $PackageDirectory "${PackageName}Install.zip") "${PackageDirectory}"
+ 
+Write-Host "Moving files from ${PackageDirectory}\${PackageName}-*\* to ${PackageDirectory}"
+ 
+Copy-Item "${PackageDirectory}\${PackageName}-*\*" "${PackageDirectory}" -Force -Recurse
+ 
+ 
+Write-Host "Cleaning up temp files & directories created during this install"
+Remove-Item "${PackageDirectory}\${PackageName}*" -Recurse -Force
+Remove-Item "${PackageDirectory}\${PackageName}Install*" -Recurse -Force
---
+++ AutomaticPackages/kibana/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$package_name="Kibana"
+$package_dir="$env:ProgramFiles\$package_name"
+
+Write-Host "Uninstalling $package_name"
+Remove-Item $package_dir -Recurse -Force
