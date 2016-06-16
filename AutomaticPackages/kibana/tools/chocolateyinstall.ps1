0508dd7e7f19b3174baf9bf65a97e18f1334338c
hambone124 <hperez@tervis.com>
2016-06-16 14:13:47 -0400

    Revamped install process to use more built-in Chocolatey Helpers

--- AutomaticPackages/kibana/tools/chocolateyinstall.ps1
+++ AutomaticPackages/kibana/tools/chocolateyinstall.ps1
@@ -1,37 +1,10 @@
-﻿<#
-Script modified from Dmitry Kireev's original script.
-https://chocolatey.org/packages/logstash-contrib
-#>
-
-
-$PackageName = '{{PackageName}}'
+﻿$PackageName = '{{PackageName}}'
 $PackageVersion = '{{PackageVersion}}'
 $ServiceName = 'kibana-service'
-$PackageDirectory="$env:ChocolateyInstall\lib\$PackageName"
+$PackageDirectory = $env:chocolateyPackageFolder
 $DownloadURL = "http://download.elastic.co/$PackageName/$PackageName/$PackageName-$PackageVersion-windows.zip"
 
-Write-Host "Making sure there are no traces of an old installation. Cleaning up $PackageDirectory."
-
-$DoesPackagePathExist = Test-Path $(Join-Path $PackageDirectory "$PackageName*")
-
-if ($DoesPackagePathExist -eq $true) {
-    Get-ChildItem "${PackageDirectory}" | Where-Object {$_.Name -ilike "$PackageName*"} | Remove-Item -Recurse
-    Get-ChildItem "${PackageDirectory}" | Where-Object {$_.Name -ilike "${PackageName}Install*"} | Remove-Item
-}
-else {
-    Write-Host "No old files to remove."
-}
-
-Get-ChocolateyWebFile "${PackageName}" $(Join-Path $PackageDirectory "${PackageName}Install.zip") $DownloadURL $DownloadURL
-Get-ChocolateyUnzip $(Join-Path $PackageDirectory "${PackageName}Install.zip") "${PackageDirectory}"
- 
-Write-Host "Moving files from ${PackageDirectory}\${PackageName}-*\* to ${PackageDirectory}"
- 
-Copy-Item "${PackageDirectory}\${PackageName}-*\*" "${PackageDirectory}" -Force -Recurse 
- 
-Write-Host "Cleaning up temp files & directories created during this install"
-Remove-Item "${PackageDirectory}\${PackageName}*" -Recurse -Force
-Remove-Item "${PackageDirectory}\${PackageName}Install*" -Recurse -Force
+Install-ChocolateyZipPackage -PackageName $PackageName -UnzipLocation $PackageDirectory -Url $DownloadURL
 
 Write-Host "Installing service"
 
--- AutomaticPackages/kibana/tools/chocolateyuninstall.ps1
+++ AutomaticPackages/kibana/tools/chocolateyuninstall.ps1
@@ -1,6 +1,5 @@
 ﻿$PackageName="Kibana"
 $ServiceName="kibana-service"
-$PackageDirectory="$(Join-Path "$env:ChocolateyInstall\lib" $PackageName)"
 
 if ($Service = Get-Service $ServiceName -ErrorAction SilentlyContinue) {
     Write-Host "Removing service."
@@ -9,6 +8,3 @@ if ($Service = Get-Service $ServiceName -ErrorAction SilentlyContinue) {
     }
     Start-ChocolateyProcessAsAdmin "delete $ServiceName" "sc.exe"
 }
-
-Write-Host "Uninstalling $PackageName"
-Remove-Item $PackageDirectory -Recurse -Force
