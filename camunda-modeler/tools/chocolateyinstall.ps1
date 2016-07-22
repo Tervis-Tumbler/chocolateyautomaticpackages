b90443674b46f5e4ce3c0b4349baaee521f5743a
hambone124 <hambone124@users.noreply.github.com>
2016-07-22 18:13:22 -0400

    Added Set-PathVersion
    Added Set-PathVersion to fix download URL when version ends in "0".

--- camunda-modeler/tools/chocolateyinstall.ps1
+++ camunda-modeler/tools/chocolateyinstall.ps1
@@ -1,6 +1,20 @@
-﻿$PackageName = '{{PackageName}}'
+$PackageName = '{{PackageName}}'
 $PackageVersion = '{{PackageVersion}}'
 
-$DownloadURL = "https://camunda.org/release/$PackageName/$PackageVersion/$PackageName-$PackageVersion-win32-x64.zip"
+function Set-PathVersion {
+    $VersionSplit = $PackageVersion.Split(".")
+    if ($VersionSplit[2] -eq "0") {
+        $PathVersion = [string]::Join(".",$($VersionSplit[0,1]))
+    }
+    else {
+        $PathVersion = $PackageVersion
+    }
+
+    return $PathVersion
+}
+
+$PathVersionNumber = Set-PathVersion
+
+$DownloadURL = "https://camunda.org/release/$PackageName/$PathVersionNumber/$PackageName-$PackageVersion-win32-x64.zip"
 
 Install-ChocolateyZipPackage -PackageName $PackageName -UnzipLocation $env:chocolateyPackageFolder -Url $DownloadURL
