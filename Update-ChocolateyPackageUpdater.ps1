fdcc58e6b9343434e9133116a116120ebcb0c7a8
hambone124 <hambone124@users.noreply.github.com>
2016-07-25 18:32:50 -0400

    Create Update-ChocolateyPackageUpdater.ps1

---
+++ Update-ChocolateyPackageUpdater.ps1
@@ -0,0 +1,21 @@
+choco update chocolateypackageupdater -y
+
+Set-Location -Path C:\tools\ChocolateyPackageUpdater\
+
+Move-Item -Path .\chocopkgup.exe.config -Destination .\chocopkgup.exe.config.bak -Force
+
+$ChocoPkgUpConfig = @"
+<?xml version="1.0"?>
+<configuration>
+  <appSettings>
+    <add key="PackagesFolder" value="C:\GitHub\chocolateyautomaticpackages"/>
+    <add key="TokenReplaceFileSearchPattern" value="*.txt|*.nuspec|*.ps1|*.config"/>
+    <add key="PS1FileToExecute" value="ChocoPkgUp.ps1"/>
+  </appSettings>
+  <startup>
+    <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.0"/>
+  </startup>
+</configuration>
+"@
+
+$ChocoPkgUpConfig > chocopkgup.exe.config
