910e8b43a8d25dfd01c053d540a5c13d3305aa9c
hambone124 <hpe60m5@gmail.com>
2016-08-16 17:48:57 -0400

    Initial commit for Office365 Chocolatey package

---
+++ Static/Office365.2016/tools/chocolateyinstall.ps1
@@ -0,0 +1,104 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'Office365.2016'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_7213-5776.exe' # download url
+$installConfigFileLocation = $(Join-Path $toolsDir 'install.xml')
+$uninstallConfigFileLocation = $(Join-Path $toolsDir 'uninstall.xml')
+$ignoreExtractFile = "officedeploymenttool.exe.ignore"
+$ignoreSetupFile = "setup.exe.ignore"
+$arch = 32
+$sharedMachine = 0
+$logPath = "%TEMP%"
+
+$arguments = @{}
+
+$packageParameters = $env:chocolateyPackageParameters
+
+if ($packageParameters) {
+    $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z0-9]+))"
+    $option_name = 'option'
+    $value_name = 'value'
+
+    if ($packageParameters -match $match_pattern ){
+        $results = $packageParameters | Select-String $match_pattern -AllMatches
+        $results.matches | % {
+            $arguments.Add(
+                $_.Groups[$option_name].Value.Trim(),
+                $_.Groups[$value_name].Value.Trim())
+        }
+    }
+    else
+    {
+        Throw "Package Parameters were found but were invalid (REGEX Failure)"
+    }
+
+    if ($arguments.ContainsKey("64bit")) {
+        Write-Host "Installing 64-bit version."
+        $arch = 64
+    } else {
+        Write-Host "Installing 32-bit version."
+    }
+
+    if ($arguments.ContainsKey("Shared")) {
+        Write-Host "Installing with Shared Computer Licensing for Remote Desktop Services."
+        $sharedMachine = 1
+    }
+
+    if ($arguments.ContainsKey("LogPath")) {
+        Write-Host "Installation log in directory $($arguments['LogPath'])"
+        $logPath = $arguments["LogPath"]
+    }
+
+} else {
+    Write-Debug "No Package Parameters Passed in"
+    Write-Host "Installing 32-bit version."
+    Write-Host "Installation log in directory %TEMP%"
+}
+
+$installConfigData = @"
+<Configuration>
+  <Add OfficeClientEdition="$arch">
+    <Product ID="O365ProPlusRetail">
+      <Language ID="en-us" />
+    </Product>
+  </Add>  
+  <Display Level="None" AcceptEULA="TRUE" />  
+  <Logging Level="Standard" Path="$logPath" /> 
+  <Property Name="SharedComputerLicensing" Value="$sharedMachine" />  
+</Configuration>
+"@
+
+$uninstallConfigData = @"
+<Configuration>
+  <Remove>
+    <Product ID="O365ProPlusRetail">
+      <Language ID="en-us" />
+    </Product>
+  </Remove>
+  <Display Level="None" AcceptEULA="TRUE" />  
+  <Logging Level="Standard" Path="$logPath" /> 
+  <Property Name="FORCEAPPSHUTDOWN" Value="True" />
+</Configuration>
+"@
+
+$installConfigData | Out-File $installConfigFileLocation
+$uninstallConfigData | Out-File $uninstallConfigFileLocation
+
+New-Item -Path $toolsDir -Name $ignoreSetupFile
+New-Item -Path $toolsDir -Name $ignoreExtractFile
+
+$extractPackage = Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$toolsDir\officedeploymenttool.exe" -Url $url
+
+$statements = "/extract:$toolsDir /quiet /norestart"
+
+$setupPackageArgs = @{
+  packageName = $packageName
+  fileType = 'EXE'
+  file = $(Join-Path $toolsDir "setup.exe")
+  silentArgs = "/configure $installconfigFileLocation"
+}
+
+Start-ChocolateyProcessAsAdmin -Statements "$statements" -ExeToRun $extractPackage
+
+Install-ChocolateyInstallPackage @setupPackageArgs
---
+++ Static/Office365.2016/tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$uninstallConfigFileLocation = $(Join-Path $toolsDir 'uninstall.xml')
+$deploymentTool = $(Join-Path $toolsDir 'setup.exe')
+$silentArgs = "/configure $uninstallConfigFileLocation"
+$installerType = 'EXE'
+$packageName = 'Office365.2016'
+
+Uninstall-ChocolateyPackage -PackageName $packageName `
+                            -FileType $installerType `
+                            -SilentArgs "$silentArgs" `
+                            -File "$deploymentTool"
