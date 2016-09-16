987964219f4f6ba5aaa02a2555b810e4e709afcd
hambone124 <hperez@tervis.com>
2016-09-16 16:48:33 -0400

    Created package for Cisco Jabber

---
+++ Static/CiscoJabber/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'CiscoJabber'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = "\\fs1\DisasterRecovery\Programs\Cisco\Cisco Phone Software\Jabber\Jabber for Windows\Jabber 11.6.1\CiscoJabberSetup.msi"
+
+$packageArgs = @{
+  packageName = $packageName
+  unzipLocation = $toolsDir
+  fileType = 'msi'
+  file = $fileLocation
+
+  softwareName  = 'Cisco Jabber*'
+
+  checksum      = 'E6B79BDBC013F10160FB192E9632D0E2'
+  checksumType  = 'md5'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
