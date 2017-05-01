d3c0a9e2de6d5dc695152b7b3620e1c2204d9915
hambone124 <hpe60m5@gmail.com>
2017-05-01 14:26:25 -0400

    SQL Anywhere

---
+++ Static/SQLAnywhere12/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = "SQLAnywhere12"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = "http://d5d4ifzqzkhwt.cloudfront.net/sqla12client/SQLA1201_Client.exe"
+
+$downloadArgs = @{
+    packageName = $packageName
+    fileFullPath = "$toolsDir\SA12.exe"
+    url = $url
+    checksum = "B4288B1ACB939B30BEAA61323666A280A31E8F32D7147A556804425E8106DAED"
+    checksumType = "SHA256"
+}
+
+$packageArgs = @{
+    packageName = $packageName
+    file = "$toolsDir\SA12\setup.exe"
+    fileType = 'EXE'
+    silentArgs = '/S "/v /qn SA32=1 SA64=1"'
+}
+
+Get-ChocolateyWebFile @downloadArgs
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\SA12.exe" -Destination "$toolsDir\SA12\"
+Install-ChocolateyInstallPackage @packageArgs
