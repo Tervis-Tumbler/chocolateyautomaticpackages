46f3edca802d757521d766e128f75e10acb5f104
Hamilton Perez <hambone124@users.noreply.github.com>
2017-11-29 08:43:10 -0500

    TeamViewer 12 MSI

--- Static/teamviewer12msi/tools/chocolateyinstall.ps1
+++ Static/teamviewer12msi/tools/chocolateyinstall.ps1
@@ -1,17 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'teamviewermsi'
+$packageName= 'teamviewer12msi'
+$packageVersion = "12.0.88438"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = "\\fs1\DisasterRecovery\Programs\Teamviewer\Teamviewer 9 Full\TeamViewer-idcg83r3f6.msi"
+$fileLocation = "$toolsDir\TeamViewer-idcg83r3f6.msi"
 
 $packageArgs = @{
   packageName = $packageName
   unzipLocation = $toolsDir
   fileType = 'msi'
   file = $fileLocation
-  softwareName  = 'Teamviewer 9'
-  checksum      = 'd5d5dc26c4b931023ae8d79a4936a417'
-  checksumType  = 'md5'
+  softwareName  = 'Teamviewer 12'
+  checksum      = '0A60DC5207CF666F9B9BB2DCEC9F8E526F60338018EEF743FA4E86FA517CFD8F'
+  checksumType  = 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
